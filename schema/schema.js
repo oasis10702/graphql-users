1f11a42118caabe81b4930fce0df8a2e59d19c27
oasis10702 <oasis10702@gmail.com>
2021-06-16 19:41:59 +0900

    Write graphql schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,11 @@
-// TODO
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { tpye: GraphQLInt }
+  }
+});
