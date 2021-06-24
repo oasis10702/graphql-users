2a7d1f98e5b50dc7ac2424b4e2f35d14add95d21
oasis10702 <oasis10702@gmail.com>
2021-06-24 22:20:31 +0900

    Fix query

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,10 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const _ = require('lodash');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLNonNull } = graphql;
 
 const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 }
+  { id: '1', firstName: 'Bill', age: 20 },
+  { id: '2', firstName: 'Samantha', age: 21 }
 ];
 
 const UserType = new GraphQLObjectType({
@@ -11,20 +12,26 @@ const UserType = new GraphQLObjectType({
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { tpye: GraphQLInt }
+    age: { type: GraphQLInt }
   }
 });
+
+const UserQuery = {
+  type: UserType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLString) }
+  },
+  resolve(parentValue, args) {
+    console.log('args', args);
+    console.log('users', users);
+    return _.find(users, { id: args.id });
+  }
+};
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    user: {
-      type: UserType,
-      args: { id: { type: GraphQLString } },
-      resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
-      }
-    }
+    user: UserQuery
   }
 });
 
