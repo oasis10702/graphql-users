3c5a868674bb583472b9a9f47619ddb4f9206a23
oasis10702 <oasis10702@gmail.com>
2021-09-10 19:07:54 +0900

    Implement

--- schema/schema.js
+++ schema/schema.js
@@ -86,7 +86,13 @@ const mutation = new GraphQLObjectType({
     editUser: {
       type: UserType,
       args: {
-        id: { type: new GraphQLNonNull(GraphQLString) }
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args).then(res => res.data);
       }
     }
   }
